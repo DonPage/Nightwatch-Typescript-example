5ac9bd897743ad7cb6327dd13fdce6cd51cd697a
Donald Page <don.page.work@gmail.com>
2016-03-28 09:21:54 -0400

    ADDED: more steps to aboutPage.ts

--- src/tests/aboutPage.ts
+++ src/tests/aboutPage.ts
@@ -1,14 +1,22 @@
 import sitemap from '../misc/sitemap';
 import {Tags} from '../misc/tags';
 import {NavBar, NavMenu} from '../components/globals'
-console.log('Tags.smoke ', Tags.smoke)
+
 let tests = {
+
   '@tags': [Tags.smoke, Tags.sprint1],
-  'Nav Menu Links': client => {
+
+  'Open Homepage': client => {
     client
       .url(sitemap.home.url)
       .waitForElementVisible('body', 1000);
+  },
+
+  'Open Nav': client => {
     NavBar.commands['openNavMenu'](client);
+  },
+
+  'Goto About Page': client => {
     NavMenu.commands['clickLink'](client, '/about', () => client.assert.urlContains(sitemap.about.url).end());
   }
 
