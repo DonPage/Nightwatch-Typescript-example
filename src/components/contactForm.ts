3c80472780826917bf09d2042b1bd8f98769c4bb
Donald Page <don.page.work@gmail.com>
2016-04-15 14:57:56 -0400

    ADDED: injection.ts and contactForm.ts component.

---
+++ src/components/contactForm.ts
@@ -0,0 +1,9 @@
+
+export class $Form {
+  constructor(public client: any) {}
+  exit: string = '.btn-close';
+  back: string = '.btn-prev';
+  next: string = '.btn-next';
+  activeInput: string = 'div.form-panel.active > form-panel-content > input';
+  
+}
--- src/tests/security.ts
+++ src/tests/security.ts
@@ -1,25 +1,24 @@
 import sitemap from '../misc/sitemap';
 import {takeScreenshot} from '../commands/util.commands';
 
+const hiddenDirectories =['/readme', '/readme.md', ''];
+
 let tests = {
   before: client => client.url(sitemap.home).waitForElementVisible('body.unlocked'),
   after: client => client.end(),
 
   '@tags': ['checklist'],
 
-  '404': client => {
-    client.url(`${sitemap.home}/Testing404Page`)
+  'README': client => { //README should not be visible, should redirect to 404 page.
+    client.url(`${sitemap.home}/README.md`)
       .waitForElementVisible('body.loaded').perform((client, done) => {
-      takeScreenshot(client, '404-Page', {key: 'DATE', value: new Date()})
+      takeScreenshot(client, '404-Page', {'DATE': new Date()})
         .then(() => {
           client.assert.urlContains('404');
           done();
         });
     });
-  },
 
-  'FavIcon': client => {
-    client.expect.element('link').to.have.attribute('href');
   }
 
 };
